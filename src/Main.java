ef7e5932cf85fcc035909f5d765f90099279c117
Imane Ouabou <137705693+Imaneouabou@users.noreply.github.com>
2024-11-10 23:03:34 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Idsit 3A!");
           System.out.println("EDIT IMANE OUABOU!");
+
+           System.out.println("EDIT IMANE OUABOU! 2");
     }
 }
